8981820b10e4a298ed08fea4679b7960951dbc6b
HungNP <21520252@gm.uit.edu.vn>
2024-01-13 20:22:36 +0700

    init

---
+++ src/main/java/me/phuochung/greenmart/GreenMartApplication.java
@@ -0,0 +1,13 @@
+package me.phuochung.greenmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenMartApplication.class, args);
+    }
+
+}
