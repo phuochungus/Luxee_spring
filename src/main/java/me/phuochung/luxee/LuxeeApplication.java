049ab43347474477683b6d748b3eaf1f0bdd442b
HungNP <21520252@gm.uit.edu.vn>
2024-02-01 21:00:23 +0700

    change package name -> luxee

--- src/main/java/me/phuochung/luxee/LuxeeApplication.java
+++ src/main/java/me/phuochung/luxee/LuxeeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GreenMartApplication {
+public class LuxeeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GreenMartApplication.class, args);
+        SpringApplication.run(LuxeeApplication.class, args);
     }
 }
